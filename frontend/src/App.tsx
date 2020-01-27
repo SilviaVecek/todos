2adf2c773eea07d2af3e65fe0b5322686cb859d0
Silvia Feng <silvia.feng@mediushealth.org>
2020-01-28 09:06:32 +1100

    built frontend list, input and fetch for patch, delete, add and get

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,85 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
-import { Input } from 'antd';
-import { List } from 'antd';
-import { getTodos } from './api/todo';
+import { Input, List, Icon, Row, Col, Button } from 'antd';
+import { getTodos, newTodo, removeTodo, updateTodo } from './api/todo';
 import { Todo } from './types/todo';
-const { Search } = Input;
+import { findByLabelText } from '@testing-library/react';
+const ButtonGroup = Button.Group;
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [input, setInput] = useState(false);
   useEffect(() => {
     (async () => {
       const data = await getTodos()   
       setTodos(data.todos)
     })();
   }, [])
+
+  const addTodo = async e => {
+    const response = await newTodo({description: e.target.value});
+    let spreadTodos = [...todos, response];
+    setTodos(spreadTodos);
+    setInput(false);
+  };
+
+  const deleteTodo = (id) => {
+    removeTodo({id: id, complete: true});
+    setTodos(todos.filter(item => item.id !== id));
+  }
+
+  const markCompleted = async (id, complete) => {
+    const response = await updateTodo({id: id, complete: !complete});
+    setTodos([...todos.filter(item => item.id !== id), response])
+  }
+
+  const showInput = () => {
+    setInput(true)
+  }
+
+  const closeInput = () => {
+    setInput(false)
+  }
+
+  const showComplete = () => {
+    setTodos(todos.filter(item => item.complete))
+  }
+
+  const showIncomplete = () => {
+    setTodos(todos.filter(item => !item.complete))
+  }
+
+  const sortedTodos = [...todos].sort((a, b) => a.id > b.id ? 1 : -1)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Search placeholder="input search text" onSearch={value => console.log(value)} enterButton />
-        <List
-          size="large"
-          header={<div>To Do List</div>}
-          footer={<div>Add another todo...</div>}
-          bordered
-          dataSource={todos.map(todo => todo.description)}
-          renderItem={item => <List.Item>{item}</List.Item>}
-        />
-      </header>
+      <Row type="flex" justify="center" align="middle" style={{ minHeight: '100vh' }}>
+        <Col md={12}>
+          <List
+            size="large"
+            header={<div className="App-header">To Do List</div>}
+            bordered
+            dataSource={sortedTodos}
+            renderItem={item => 
+            <List.Item style={{display: 'flex', textDecoration: item.complete ? 'line-through' : 'none'}}>
+              <Button icon={item.complete ? "close" : "none"} style={{fontSize: '10px', padding: '0px', height: '18px', width: '18px', marginRight: '10px'}} onClick={() => markCompleted(item.id, item.complete)}>
+              </Button>{item.description}
+            </List.Item>}
+          />
+          {input &&
+          <>
+            <Input autoFocus size="large" placeholder="create a todo..." allowClear onPressEnter={addTodo}/>
+            <Button style={{position: 'absolute', fontSize: '25px', marginLeft: '10px'}} type="link" icon="close-circle" shape="circle" onClick={closeInput}/>
+          </>}
+          <Button type="link" style={{fontSize: '30px', display: 'flex', margin: '20px auto 0px auto'}}icon="plus-circle" shape="circle" onClick={showInput}/>
+          <ButtonGroup style={{display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
+            <Button onClick={showIncomplete}>Show incomplete</Button>
+            <Button onClick={showComplete}>Show complete</Button>
+            <Button onClick={deleteTodo}>Remove All Completed Todos</Button>
+          </ButtonGroup>        
+        </Col>  
+      </Row>  
     </div>
   );
 }
--- frontend/src/api/todo.ts
+++ frontend/src/api/todo.ts
@@ -1,15 +1,69 @@
 import { API } from "../constants";
 import { Todo } from "../types/todo";
 
+// Get all todos
 export async function getTodos(): Promise<{ todos: Todo[] }> {
     try {
-    let response = await fetch(`${API}/todos`, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+    let response = await fetch(`${API}/todos`)
     return await response.json();
     } catch(err) {
       return err;
     }
   }
+
+
+// Create new todo
+export async function newTodo(todo): Promise<Todo> {
+  try {
+    let response = await fetch(`${API}/todos`, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify({
+        description: todo.description,
+        complete: false,
+      })
+    })
+    return await response.json();
+  } catch(err) {
+    return err
+  }
+}
+
+// Delete todo
+export async function removeTodo(todo): Promise<{ todos: Todo[]}> {
+  try {
+    let response = await fetch(`${API}/todos/${todo.id}`, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: "DELETE",
+      body: JSON.stringify({
+        description: todo.description,
+        complete: true,
+      })
+    })
+    return await response.json();
+  } catch(err) {
+    return err
+  }
+}
+
+// Update todo
+export async function updateTodo(todo): Promise<Todo> {
+  try {
+    let response = await fetch(`${API}/todos/${todo.id}`, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: "PATCH",
+      body: JSON.stringify({
+        complete: todo.complete,
+      })
+    })
+    return await response.json();
+  } catch(err) {
+    return err
+  }
+}
